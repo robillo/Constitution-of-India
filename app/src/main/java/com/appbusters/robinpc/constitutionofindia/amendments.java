23b22e1337141690ab2958e2e61ed929c42838ed
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-07-08 17:29:46 +0530

    refer Parts and part1, note that some parts have aabout 50-100 articles

--- app/src/main/java/com/appbusters/robinpc/constitutionofindia/amendments.java
+++ app/src/main/java/com/appbusters/robinpc/constitutionofindia/amendments.java
@@ -3,6 +3,7 @@ package com.appbusters.robinpc.constitutionofindia;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,5 +25,12 @@ public class amendments extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void Amendment1(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.large_text).create();
+        builder.show();
     }
 }
